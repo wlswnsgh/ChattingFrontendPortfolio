09ba1c059244f8cd581202137b41ecbf89cb636e
jin <junho11_kr@naver.com>
2024-08-21 22:37:07 +0900

    up

---
+++ src/Chatting/login/Password.jsx
@@ -0,0 +1,12 @@
+// https://simplecaptcha.sourceforge.net/ 자동방지입력 사이트
+import React from 'react';
+
+function Password() {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Password;
