23e8ffbedebb2c25b0d4762771e66351b28849e3
jin <junho11_kr@naver.com>
2024-08-22 18:32:04 +0900

    up

--- src/Chatting/login/ID.jsx
+++ src/Chatting/login/ID.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+const Container = styled.div`
+    position: absolute;
+    width: 35rem;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+`;
+
 function ID() {
     return (
-        <>
-            
-        </>
+        <Container>
+
+        </Container>
     );
 }
 
